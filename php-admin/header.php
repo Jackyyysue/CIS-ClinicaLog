5100d375f9f8ea02e7e1d61f3e0ba09b7179242a
Jackyyysue <jmfurog00473@usep.edu.ph>
2024-10-11 23:13:31 +0800

    Update header.php

--- php-admin/header.php
+++ php-admin/header.php
@@ -73,7 +73,7 @@ $userData = $user->getUserData($user_id);
     <div class="main-header-logo">
             <!-- Logo Header -->
             <div class="logo-header" data-background-color="dark">
-              <a href="index.html" class="logo">
+              <a href="index.php" class="logo">
                 <img
                   src="../assets/img/kaiadmin/logo_light.svg"
                   alt="navbar brand"
