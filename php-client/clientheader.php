1623cce38ca87915d848f63500334b2db075679a
Jackyyysue <jmfurog00473@usep.edu.ph>
2024-10-27 13:37:53 +0800

    Commit

--- php-client/clientheader.php
+++ php-client/clientheader.php
@@ -1,15 +1,3 @@
-<?php
-session_start();
-include('../database/config.php');
-include('../php/user.php');
-
-$db = new Database();
-$conn = $db->getConnection();
-
-$user = new User($conn);
-$user_id = $_SESSION['user_id'];
-$userData = $user->getUserData($user_id);
-?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -65,7 +53,7 @@ $userData = $user->getUserData($user_id);
           alt="navbar brand"
           class="navbar-brand"
           height="30" />
-          ClinicaLog
+        ClinicaLog
       </a>
       <div class="nav-toggle">
       </div>
@@ -79,6 +67,13 @@ $userData = $user->getUserData($user_id);
   <nav
     class="navbar navbar-header navbar-header-transparent navbar-expand-lg border-bottom">
     <div class="container-fluid">
+      <a href="clientindex.php" class="logo">
+        <img
+          src="../assets/img/sidebar-logo.svg"
+          alt="navbar brand"
+          class="navbar-brand"
+          height="60" />
+      </a>
       <ul class="navbar-nav topbar-nav ms-md-auto align-items-center">
         <li>
           <nav
@@ -164,7 +159,7 @@ $userData = $user->getUserData($user_id);
                   <a href="#">
                     <div class="notif-img">
                       <img
-                        src="../assets/img/profile2.jpg"
+                        src="assets/img/profile2.jpg"
                         alt="Img Profile" />
                     </div>
                     <div class="notif-content">
@@ -192,100 +187,23 @@ $userData = $user->getUserData($user_id);
             </li>
           </ul>
         </li>
-        <li class="nav-item topbar-icon dropdown hidden-caret">
+        <li class="nav-item topbar-user dropdown hidden-caret">
           <a
-            class="nav-link"
+            class="dropdown-toggle profile-pic"
             data-bs-toggle="dropdown"
             href="#"
             aria-expanded="false">
-            <i class="fas fa-layer-group"></i>
+            <div class="avatar-sm">
+              <img
+                src="../assets/img/profile.jpg"
+                alt="..."
+                class="avatar-img rounded-circle" />
+            </div>
+            <span class="profile-username">
+              <span class="op-7">Hi,</span>
+              <span class="fw-bold">Jacky</span>
+            </span>
           </a>
-          <div class="dropdown-menu quick-actions animated fadeIn">
-            <div class="quick-actions-header">
-              <span class="title mb-1">Quick Actions</span>
-              <span class="subtitle op-7">Shortcuts</span>
-            </div>
-            <div class="quick-actions-scroll scrollbar-outer">
-              <div class="quick-actions-items">
-                <div class="row m-0">
-                  <a class="col-6 col-md-4 p-0" href="#">
-                    <div class="quick-actions-item">
-                      <div class="avatar-item bg-danger rounded-circle">
-                        <i class="far fa-calendar-alt"></i>
-                      </div>
-                      <span class="text">Calendar</span>
-                    </div>
-                  </a>
-                  <a class="col-6 col-md-4 p-0" href="#">
-                    <div class="quick-actions-item">
-                      <div
-                        class="avatar-item bg-warning rounded-circle">
-                        <i class="fas fa-map"></i>
-                      </div>
-                      <span class="text">Maps</span>
-                    </div>
-                  </a>
-                  <a class="col-6 col-md-4 p-0" href="#">
-                    <div class="quick-actions-item">
-                      <div class="avatar-item bg-info rounded-circle">
-                        <i class="fas fa-file-excel"></i>
-                      </div>
-                      <span class="text">Reports</span>
-                    </div>
-                  </a>
-                  <a class="col-6 col-md-4 p-0" href="#">
-                    <div class="quick-actions-item">
-                      <div
-                        class="avatar-item bg-success rounded-circle">
-                        <i class="fas fa-envelope"></i>
-                      </div>
-                      <span class="text">Emails</span>
-                    </div>
-                  </a>
-                  <a class="col-6 col-md-4 p-0" href="#">
-                    <div class="quick-actions-item">
-                      <div
-                        class="avatar-item bg-primary rounded-circle">
-                        <i class="fas fa-file-invoice-dollar"></i>
-                      </div>
-                      <span class="text">Invoice</span>
-                    </div>
-                  </a>
-                  <a class="col-6 col-md-4 p-0" href="#">
-                    <div class="quick-actions-item">
-                      <div
-                        class="avatar-item bg-secondary rounded-circle">
-                        <i class="fas fa-credit-card"></i>
-                      </div>
-                      <span class="text">Payments</span>
-                    </div>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </li>
-        <li class="nav-item topbar-user dropdown hidden-caret">
-          <?php if ($userData): ?>
-            <a
-              class="dropdown-toggle profile-pic"
-              data-bs-toggle="dropdown"
-              href="#"
-              aria-expanded="false">
-              <div class="avatar-sm">
-                <img
-                  src='/php-admin/uploads/<?php echo htmlspecialchars($userData['user_profile']); ?>'
-                  alt='Profile Picture'
-                  class="avatar-img rounded-circle" />
-              </div>
-              <span class="profile-username">
-                <span class="op-7">Hi,</span>
-                <span class="fw-bold"><?php echo htmlspecialchars($userData['user_fname']); ?></span>
-              </span>
-            </a>
-          <?php else: ?>
-            <p>User data not found.</p>
-          <?php endif; ?>
           <ul class="dropdown-menu dropdown-user animated fadeIn">
             <div class="dropdown-user-scroll scrollbar-outer">
               <li>
@@ -297,23 +215,14 @@ $userData = $user->getUserData($user_id);
                       class="avatar-img rounded" />
                   </div>
                   <div class="u-text">
-                    <h4>Hizrian</h4>
-                    <p class="text-muted">hello@example.com</p>
-                    <a
-                      href="profile.html"
-                      class="btn btn-xs btn-secondary btn-sm">View Profile</a>
-                  </div>
+                    <h4>Jacky</h4>
+                    <p class="text-muted">jmfurog@usep.edu.ph</p>
                 </div>
               </li>
               <li>
+                <a class="dropdown-item" href="clientprofile.php">Account Setting</a>
                 <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#">My Profile</a>
-                <a class="dropdown-item" href="#">My Balance</a>
-                <a class="dropdown-item" href="#">Inbox</a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#">Account Setting</a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#">Logout</a>
+                <a class="dropdown-item" href="clientindex.php">Logout</a>
               </li>
             </div>
           </ul>
--- php-client/clientindex.php
+++ php-client/clientindex.php
@@ -1,161 +1,155 @@
-  <!DOCTYPE html>
-  <html lang="en">
+<!DOCTYPE html>
+<html lang="en">
 
-  <head>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <title>Client Panel</title>
-    <meta content="width=device-width, initial-scale=1.0, shrink-to-fit=no" name="viewport" />
-    <link rel="icon" href="../assets/img/ClinicaLog.ico" type="image/x-icon" />
+<head>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <title>Client Panel</title>
+  <meta content="width=device-width, initial-scale=1.0, shrink-to-fit=no" name="viewport" />
+  <link rel="icon" href="../assets/img/ClinicaLog.ico" type="image/x-icon" />
 
-    <!-- Fonts and icons -->
-    <script src="../assets/js/plugin/webfont/webfont.min.js"></script>
-    <script>
-      WebFont.load({
-        google: {
-          families: ["Public Sans:300,400,500,600,700"]
-        },
-        custom: {
-          families: [
-            "Font Awesome 5 Solid",
-            "Font Awesome 5 Regular",
-            "Font Awesome 5 Brands",
-            "simple-line-icons",
-          ],
-          urls: ["../css/fonts.min.css"],
-        },
-        active: function() {
-          sessionStorage.fonts = true;
-        },
-      });
-    </script>
+  <!-- Fonts and icons -->
+  <script src="../assets/js/plugin/webfont/webfont.min.js"></script>
+  <script>
+    WebFont.load({
+      google: {
+        families: ["Public Sans:300,400,500,600,700"]
+      },
+      custom: {
+        families: ["Font Awesome 5 Solid", "Font Awesome 5 Regular", "Font Awesome 5 Brands", "simple-line-icons"],
+        urls: ["../css/fonts.min.css"],
+      },
+      active: function() {
+        sessionStorage.fonts = true;
+      },
+    });
+  </script>
 
-    <!-- CSS Files -->
-    <link rel="stylesheet" href="../css/bootstrap.min.css" />
-    <link rel="stylesheet" href="../css/plugins.min.css" />
-    <link rel="stylesheet" href="../css/kaiadmin.min.css" />
-    <link rel="stylesheet" href="../css/client.css">
+  <!-- CSS Files -->
+  <link rel="stylesheet" href="../css/bootstrap.min.css" />
+  <link rel="stylesheet" href="../css/plugins.min.css" />
+  <link rel="stylesheet" href="../css/kaiadmin.min.css" />
+  <link rel="stylesheet" href="../css/client.css">
 
-    <!-- ICONS -->
-    <!-- Replaced the local Font Awesome link with CDN to prevent conflicts -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-pQnI6Z1ypA1QPTDdTnYkkpN0sE+0ZK3SAs+69IXS7SgSR/RG6upgjB8cSBaHh0FYv3cwUqq3Kv1BrV3iwGsnZw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-  </head>
+  <!-- ICONS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-pQnI6Z1ypA1QPTDdTnYkkpN0sE+0ZK3SAs+69IXS7SgSR/RG6upgjB8cSBaHh0FYv3cwUqq3Kv1BrV3iwGsnZw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
 
-  <body>
-    <div class="wrapper">
-      <div class="main-panel" id="clientpanel">
-        <!-- Header -->
-        <div class="main-header" id="client_header"></div>
-        <!-- Main Content -->
-        <div class="container" id="content">
-          <div class="page-inner">
-            <!-- Modal Structure -->
-            <!-- Appointment Modal -->
-            <div class="modal fade" id="appointmentModal" tabindex="-1" aria-labelledby="appointmentModalLabel" aria-hidden="true">
-              <div class="modal-dialog">
-                <div class="modal-content">
-                  <div class="modal-header">
-                    <h5 class="modal-title" id="appointmentModalLabel">Appointment Details</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                  </div>
-                  <div class="modal-body">
-                    <p id="appointmentDetails"></p>
-                  </div>
-                  <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                  </div>
+<body>
+  <div class="wrapper">
+    <div class="main-panel" id="clientpanel">
+      <!-- Header -->
+      <div class="main-header" id="client_header"></div>
+      <!-- Main Content -->
+      <div class="container" id="content">
+        <div class="page-inner">
+          <!-- Modal Structure -->
+          <div class="modal fade" id="appointmentModal" tabindex="-1" aria-labelledby="appointmentModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="appointmentModalLabel">Appointment Details</h5>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                  <p id="appointmentDetails"></p>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                 </div>
               </div>
             </div>
-            <!-- Appointment and Calendar Integration Start -->
-            <div class="row">
-              <!-- Appointment Form Column -->
-              <div class="col-md-5 mb-4 d-flex">
-                <div class="appointment-form">
-                  <h3>Set an Appointment</h3>
-                  <form id="appointmentForm">
-                    <div class="mb-3">
-                      <label for="appointmentDate" class="form-label">Appointment Date <span class="text-danger">*</span></label>
-                      <input type="date" class="form-control" id="appointmentDate" required>
-                    </div>
-                    <div class="mb-3">
-                      <label for="appointmentTime" class="form-label">Appointment Time <span class="text-danger">*</span></label>
-                      <input type="time" class="form-control" id="appointmentTime" required>
-                    </div>
-                    <div class="mb-3">
-                      <label for="notes" class="form-label">Notes</label>
-                      <textarea class="form-control" id="notes" rows="3" placeholder="Any additional information"></textarea>
-                    </div>
-                    <button type="submit" class="btn btn-primary">Set Appointment</button>
-                  </form>
-                </div>
-              </div>
-              <!-- Calendar Column -->
-              <div class="col-md-7 d-flex">
-                <div class="calendar">
-                  <div class="calendar-header">
-                    <div class="navigation">
-                      <button id="prevMonth" aria-label="Previous Month"><i class="fas fa-chevron-left"></i></button>
-                    </div>
-                    <div class="month-year" id="monthYear"></div>
-                    <div class="navigation">
-                      <button id="nextMonth" aria-label="Next Month"><i class="fas fa-chevron-right"></i></button>
-                    </div>
-                  </div>
-                  <div class="calendar-days">
-                    <div>Sun</div>
-                    <div>Mon</div>
-                    <div>Tue</div>
-                    <div>Wed</div>
-                    <div>Thu</div>
-                    <div>Fri</div>
-                    <div>Sat</div>
-                  </div>
-                  <div class="calendar-dates" id="calendarDates">
-                    <!-- Dates will be injected here -->
-                  </div>
-                </div>
-              </div>
-            </div>
-            <!-- Appointment and Calendar Integration End -->
           </div>
+          <!-- Appointment and Calendar Integration Start -->
+          <div class="row">
+            <div class="col-md-5 mb-4 d-flex">
+              <div class="appointment-form">
+                <h3>Set an Appointment</h3>
+                <form id="appointmentForm">
+                  <div class="mb-3">
+                    <label for="appointmentDate" class="form-label">Appointment Date <span class="text-danger">*</span></label>
+                    <input type="date" class="form-control" id="appointmentDate" required>
+                  </div>
+                  <div class="mb-3">
+                    <label for="appointmentTime" class="form-label">Appointment Time <span class="text-danger">*</span></label>
+                    <input type="time" class="form-control" id="appointmentTime" required>
+                  </div>
+                  <div class="mb-3">
+                    <label for="notes" class="form-label">Notes</label>
+                    <textarea class="form-control" id="notes" rows="3"></textarea>
+                  </div>
+                  <button type="submit" class="btn btn-primary">Set Appointment</button>
+                </form>
+              </div>
+            </div>
+            <div class="col-md-7 d-flex">
+              <div class="calendar">
+                <div class="calendar-header">
+                  <div class="navigation">
+                    <button id="prevMonth" aria-label="Previous Month"><i class="fas fa-chevron-left"></i></button>
+                  </div>
+                  <div class="month-year" id="monthYear"></div>
+                  <div class="navigation">
+                    <button id="nextMonth" aria-label="Next Month"><i class="fas fa-chevron-right"></i></button>
+                  </div>
+                </div>
+                <div class="calendar-days">
+                  <div>Sun</div>
+                  <div>Mon</div>
+                  <div>Tue</div>
+                  <div>Wed</div>
+                  <div>Thu</div>
+                  <div>Fri</div>
+                  <div>Sat</div>
+                </div>
+                <div class="calendar-dates" id="calendarDates"></div>
+              </div>
+            </div>
+          </div>
+          <!-- Appointment and Calendar Integration End -->
         </div>
       </div>
     </div>
+  </div>
 
-    <!-- Core JS Files -->
-    <script src="../assets/js/core/jquery-3.7.1.min.js"></script>
-    <script src="../assets/js/core/popper.min.js"></script>
-    <script src="../assets/js/core/bootstrap.min.js"></script>
+  <!-- Core JS Files -->
+  <script src="../assets/js/core/jquery-3.7.1.min.js"></script>
+  <script src="../assets/js/core/popper.min.js"></script>
+  <script src="../assets/js/core/bootstrap.min.js"></script>
 
-    <!-- jQuery Scrollbar -->
-    <script src="../assets/js/plugin/jquery-scrollbar/jquery.scrollbar.min.js"></script>
+  <!-- Core JS Files -->
+  <script src="../assets/js/core/jquery-3.7.1.min.js"></script>
+  <script src="../assets/js/core/popper.min.js"></script>
+  <script src="../assets/js/core/bootstrap.min.js"></script>
 
-    <!-- Chart JS -->
-    <script src="../assets/js/plugin/chart.js/chart.min.js"></script>
+  <!-- jQuery Scrollbar -->
+  <script src="../assets/js/plugin/jquery-scrollbar/jquery.scrollbar.min.js"></script>
 
-    <!-- jQuery Sparkline -->
-    <script src="../assets/js/plugin/jquery.sparkline/jquery.sparkline.min.js"></script>
+  <!-- Chart JS -->
+  <script src="../assets/js/plugin/chart.js/chart.min.js"></script>
 
-    <!-- Chart Circle -->
-    <script src="../assets/js/plugin/chart-circle/circles.min.js"></script>
+  <!-- jQuery Sparkline -->
+  <script src="../assets/js/plugin/jquery.sparkline/jquery.sparkline.min.js"></script>
 
-    <!-- Datatables -->
-    <script src="../assets/js/plugin/datatables/datatables.min.js"></script>
+  <!-- Chart Circle -->
+  <script src="../assets/js/plugin/chart-circle/circles.min.js"></script>
 
-    <!-- Bootstrap Notify -->
-    <script src="../assets/js/plugin/bootstrap-notify/bootstrap-notify.min.js"></script>
+  <!-- Datatables -->
+  <script src="../assets/js/plugin/datatables/datatables.min.js"></script>
 
-    <!-- jQuery Vector Maps -->
-    <script src="../assets/js/plugin/jsvectormap/jsvectormap.min.js"></script>
-    <script src="../assets/js/plugin/jsvectormap/world.js"></script>
+  <!-- Bootstrap Notify -->
+  <script src="../assets/js/plugin/bootstrap-notify/bootstrap-notify.min.js"></script>
 
-    <!-- Sweet Alert -->
-    <script src="../assets/js/plugin/sweetalert/sweetalert.min.js"></script>
+  <!-- jQuery Vector Maps -->
+  <script src="../assets/js/plugin/jsvectormap/jsvectormap.min.js"></script>
+  <script src="../assets/js/plugin/jsvectormap/world.js"></script>
 
-    <!-- Kaiadmin JS -->
-    <script src="../assets/js/kaiadmin.min.js"></script>
+  <!-- Sweet Alert -->
+  <script src="../assets/js/plugin/sweetalert/sweetalert.min.js"></script>
 
-    <!-- Appointment and Calendar Functionality Script -->
+  <!-- Kaiadmin JS -->
+  <script src="../assets/js/kaiadmin.min.js"></script>
+
+  <!-- Appointment and Calendar Functionality Script -->
   <script>
     $(document).ready(function() {
       // Load Header
@@ -164,7 +158,6 @@
           console.log("Error loading header: " + xhr.status + " " + xhr.statusText);
         }
       });
-
       // Calendar Functionality
       const monthYear = document.getElementById('monthYear');
       const calendarDates = document.getElementById('calendarDates');
@@ -185,7 +178,10 @@
         const month = date.getMonth();
 
         // Set month and year in header
-        const options = { month: 'long', year: 'numeric' };
+        const options = {
+          month: 'long',
+          year: 'numeric'
+        };
         monthYear.textContent = date.toLocaleDateString(undefined, options);
 
         // First day of the month
@@ -289,6 +285,6 @@
       });
     });
   </script>
-  </body>
+</body>
 
-  </html>
+</html>
